f1616bbf3c38c76532a19ea7d737a19131437fc0
Steven Liu <hliu4@stuy.edu>
2018-11-01 10:28:50 -0400

    Initialized

---
+++ dirinfo.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+void my_ls(char *dir_name){
+	
+}
+
+int main(){
+	return 0;
+}
